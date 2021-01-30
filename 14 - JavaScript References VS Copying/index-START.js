822ce155405be144335aa3c30ca01c1465b02e50
Dani Coleman <danicoleman00@gmail.com>
2021-01-30 11:15:08 -0700

    Complete Reference vs Copy lesson

--- 14 - JavaScript References VS Copying/index-START.js
+++ 14 - JavaScript References VS Copying/index-START.js
@@ -1,11 +1,26 @@
 // start with strings, numbers and booleans
+// let age = 100;
+// let age2 = age;
+// console.log(age, age2);
+// age = 200;
+// console.log(age, age2);
+//
+// let name = 'Dani';
+// let name2 = name;
+// console.log(name, name2);
+// name = 'danielle';
+// console.log(name, name2);
 
 // Let's say we have an array
 const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
 // and we want to make a copy of it.
+const team = players;
 
-// You might think we can just do something like this:
+console.log(players, team);
+// You might think we can just do something like this: if you update an array it will always reference back
+team[3] = 'Lux';
+console.log(players, team);
 
 // however what happens when we update that array?
 
@@ -18,11 +33,17 @@ const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 // So, how do we fix this? We take a copy instead!
 
 // one way
+const team2 = players.slice();
 
 // or create a new array and concat the old one in
+const team3 = [].concat(players);
 
 // or use the new ES6 Spread
+const team4 = [...players];
+team4[3] = 'heee hawww';
+console.log(team4);
 
+const team5 = Array.from(players);
 // now when we update it, the original one isn't changed
 
 // The same thing goes for objects, let's say we have a person object
@@ -33,10 +54,38 @@ const person = {
   age: 80
 };
 
+console.log(person);
+
 // and think we make a copy:
+// const captain = person;
+// captain.number = 99;
+// this changes the age
 
 // how do we take a copy instead?
+const cap2 = Object.assign({}, person, { number: 99});
+console.log(cap2);
 
 // We will hopefully soon see the object ...spread
+// const cap3 = {...person}; not in Javascript yet
 
 // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+const dani = {
+  name: 'Dani',
+  age: 40,
+  social: {
+    facebook: 'danicoleman',
+    instagram: '@djcoleman21'
+  }
+};
+
+// console.clear();
+console.log(dani);
+
+//make a copy
+const dev = Object.assign({}, dani); //this only goes one level deep (name, age) need to do a function to get into the social level (clone deep)
+dev.name = 'Danielle';
+console.log(dev)
+
+// poor mans deep clone:
+// const dev2 = JSON.parse(JSON.stringify(dani));
+// console.log(dev2)
